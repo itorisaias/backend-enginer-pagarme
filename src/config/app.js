a965bc7819e289cdf8c09f730d6b5a42efb542cd
Itor Isaias <itorsilva@saofrancisco.com.br>
2019-06-20 15:39:30 -0300

    steup swagger

--- src/config/app.js
+++ src/config/app.js
@@ -1,8 +1,14 @@
+const path = require('path')
+const swaggerUi = require('swagger-ui-express')
 const express = require('express')
 const helmet = require('helmet')
+const YAML = require('yamljs')
 const cors = require('cors')
+
 const routes = require('../api')
 
+const pahtSwagger = path.join(__dirname, './swagger', 'swagger.yaml')
+const swaggerDocument = YAML.load(pahtSwagger)
 const app = express()
 
 app.use(cors())
@@ -11,6 +17,7 @@ app.use(express.urlencoded({
   extended: true
 }))
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use('/api', routes)
 
 module.exports = app
