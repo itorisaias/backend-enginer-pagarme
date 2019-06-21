b91564eb0e982ddda5a8d6455f28c2ca09f4a5cd
Itor Isaias <itorsilva@saofrancisco.com.br>
2019-06-21 00:06:24 -0300

    correção de teste para validar o cardNumber

--- __tests__/integration/api.test.js
+++ __tests__/integration/api.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest')
 const app = require('../../src/config/app')
+const factore = require('../utils/factore')
 
 describe('API: Transactions', () => {
   describe('GET /api/v1/transactions', () => {
@@ -24,10 +25,14 @@ describe('API: Transactions', () => {
       expect(response.notFound).toBeFalsy()
     })
     it('should response with status 201', async () => {
+      const transaction = await factore.attrs('Transaction')
+      const cardNumber = transaction.card_number.slice(-4).padStart(16, '0')
       const response = await request(app)
         .post('/api/v1/transactions')
+        .send(transaction)
 
       expect(response.status).toBe(201)
+      expect(response.body.card_number).toEqual(cardNumber)
     })
   })
 })
