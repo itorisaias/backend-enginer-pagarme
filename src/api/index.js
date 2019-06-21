f3af5d769a99857be32b05eabcbab20a56f09999
Itor Isaias <itorsilva@saofrancisco.com.br>
2019-06-21 00:12:40 -0300

    atualização das rotas para consumir as controllers

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,13 @@
-const express = require('express')
+const routes = require('express').Router()
 
-const routes = express.Router()
+const TransactionController = require('../controllers/transactions')
+const { Transaction } = require('../database/models')
+
+const transactionController = new TransactionController(Transaction)
 
 routes
   .route('/v1/transactions')
-  .get((req, res) => res.send('trasanctions v1'))
-  .post((req, res) => res.status(201).send())
+  .get((req, res, next) => transactionController.index(req, res, next))
+  .post((req, res, next) => transactionController.store(req, res, next))
 
 module.exports = routes
