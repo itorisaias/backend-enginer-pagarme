bd2471c56c3ca87d1b2e0ed2e2053a2a3da13744
Itor Isaias <itorsilva@saofrancisco.com.br>
2019-06-21 18:56:27 -0300

    correção do teste para client e apikey default

--- __tests__/integration/api.test.js
+++ __tests__/integration/api.test.js
@@ -1,10 +1,13 @@
 const request = require('supertest')
 const app = require('../../src/config/app')
 const { factore, truncate } = require('../utils')
+const APIKEY = 123456789
+
 describe('API: Transactions', () => {
+  let client = null
   beforeEach(async () => {
     await truncate()
-    await factore.create('Client', { apiKey: '123456789' })
+    client = await factore.create('Client', { apiKey: APIKEY })
   })
   afterEach(async () => {
     await truncate()
@@ -14,36 +17,47 @@ describe('API: Transactions', () => {
     it('should exist router', async () => {
       const response = await request(app)
         .get('/api/v1/transactions')
-        .set('api_key', '123456789')
+        .set('api_key', APIKEY)
 
       expect(response.notFound).toBeFalsy()
     })
     it('should response with status 200', async () => {
       const response = await request(app)
         .get('/api/v1/transactions')
-        .set('api_key', '123456789')
+        .set('api_key', APIKEY)
 
       expect(response.status).toBe(200)
+    })
+    it('should return list of transactions', async () => {
+      const { id: clientId } = client
+      await factore.create('Transaction', { clientId })
+      const response = await request(app)
+        .get('/api/v1/transactions')
+        .set('api_key', APIKEY)
+
+      expect(response.body).toHaveLength(1)
     })
   })
   describe('POST /api/v1/transactions', () => {
     it('should exist router', async () => {
       const response = await request(app)
         .post('/api/v1/transactions')
-        .set('api_key', '123456789')
+        .set('api_key', APIKEY)
 
       expect(response.notFound).toBeFalsy()
     })
     it('should response with status 201', async () => {
       const transaction = await factore.attrs('Transaction')
       const cardNumber = transaction.cardNumber.slice(-4).padStart(16, '0')
+      const { id: clientId } = client
       const response = await request(app)
         .post('/api/v1/transactions')
-        .set('api_key', '123456789')
+        .set('api_key', APIKEY)
         .send(transaction)
 
       expect(response.status).toBe(201)
       expect(response.body.cardNumber).toEqual(cardNumber)
+      expect(response.body.clientId).toBe(clientId)
     })
     it('should response with 400 when patmentMethod is not allowed', async () => {
       const transaction = await factore.attrs('Transaction', {
@@ -51,7 +65,7 @@ describe('API: Transactions', () => {
       })
       const response = await request(app)
         .post('/api/v1/transactions')
-        .set('api_key', '123456789')
+        .set('api_key', APIKEY)
         .send(transaction)
 
       expect(response.status).toBe(400)
@@ -62,7 +76,7 @@ describe('API: Transactions', () => {
       delete transaction.amount
       const response = await request(app)
         .post('/api/v1/transactions')
-        .set('api_key', '123456789')
+        .set('api_key', APIKEY)
         .send(transaction)
 
       expect(response.status).toBe(400)
