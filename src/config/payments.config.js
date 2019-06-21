4c66c5c19cc55fd62ca88805bb4c45f4a25941af
Itor Isaias <itorsilva@saofrancisco.com.br>
2019-06-21 20:19:32 -0300

    create config payments

---
+++ src/config/payments.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'credit_card': {
+    percentageFee: 5,
+    status: 'waiting_funds',
+    amountDay: 30
+  },
+  'debit_card': {
+    percentageFee: 3,
+    status: 'paid',
+    amountDay: 0
+  }
+}
