1e9210a57c6cd0d64d6afac1d78dbf854712bdfe
Itor Isaias <itorsilva@saofrancisco.com.br>
2019-06-23 11:54:29 -0300

    crição de log e tratativa de error

--- src/config/server.js
+++ src/config/server.js
@@ -1,15 +1,21 @@
 const http = require('http')
 const app = require('./app')
+const log = require('../utils/logger').getInstance('server')
 
 const server = http.createServer(app)
 
 server.on('error', err => {
-  console.error(err)
+  if (err.syscall !== 'listen') {
+    throw err
+  }
+
+  log.error(err)
+  process.exit(1)
 })
 
 server.on('listening', () => {
   const { port, address } = server.address()
-  console.info(`server listening ${address}:${port}`)
+  log.info(`server listening ${address}:${port}`)
 })
 
 module.exports = {
