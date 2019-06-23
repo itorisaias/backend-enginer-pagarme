3c9a722700c57500efea3745fb1db58751f48c66
Itor Isaias <itorsilva@saofrancisco.com.br>
2019-06-23 11:35:32 -0300

    Criação de utils para log

---
+++ src/utils/logger.js
@@ -0,0 +1,45 @@
+const winston = require('winston')
+
+class Logger {
+  constructor (lib = winston) {
+    this.lib = lib
+  }
+
+  getInstance (nameService) {
+    const level = this.getLevel(process.env.NODE_ENV)
+
+    const logger = this.lib.createLogger({
+      level,
+      format: winston.format.json(),
+      defaultMeta: {
+        service: nameService
+      },
+      transports: [
+        new winston.transports.Console()
+      ]
+    })
+
+    if (['debug', 'silly'].includes(level)) {
+      // TODO add transports.File
+    }
+
+    return logger
+  }
+
+  getLevel (env) {
+    switch (env) {
+      case 'test':
+        return 'error'
+      case 'production':
+        return 'info'
+      case 'staging':
+        return 'info'
+      case 'development':
+        return 'debug'
+      default:
+        return 'silly'
+    }
+  }
+}
+
+module.exports = new Logger(winston)
