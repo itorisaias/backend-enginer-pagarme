4861ed8caa11451f060b3842d3647967dab3e2ea
Itor Isaias <itorsilva@saofrancisco.com.br>
2019-06-24 13:24:10 -0300

    correção dos testes

--- __tests__/unit/services/payables.test.js
+++ __tests__/unit/services/payables.test.js
@@ -1,48 +1,43 @@
 const moment = require('moment')
-const PayableService = require('../../../src/services/payables')
-const { factore, truncate } = require('../../utils')
+const { factore } = require('../../utils')
+const { PayableService } = require('../../../src/services')
 
 moment.locale('pt-BR')
 
 describe('Service: Payable', () => {
   describe('method: store', () => {
-    let client = null
     const paymentConfig = {
       debit_card: {
+        status: '',
         percentageFee: 5,
         amountDay: 10
       }
     }
-
-    beforeEach(async () => {
-      await truncate()
-      client = await factore.create('Client')
-    })
-    afterEach(async () => {
-      await truncate()
-    })
-
     it('should create new payable', async () => {
-      const transaction = await factore.create('Transaction', { clientId: client.id })
-      const payableModel = {
+      const transaction = await factore.attrs('Transaction', {
+        amount: 1000,
+        paymentMethod: 'debit_card',
+        transactionId: 1
+      })
+      const PayableModel = {
         create: (value) => Promise.resolve(value)
       }
-      const payableService = new PayableService(payableModel)
+      const payableService = new PayableService(PayableModel, null, paymentConfig)
 
       const payable = await payableService.store(transaction)
 
       expect(payable.transactionId).toBe(transaction.id)
     })
     it('calcular o fee corretamente com configurações customizadas', async () => {
-      const transaction = await factore.create('Transaction', {
-        clientId: client.id,
+      const transaction = await factore.attrs('Transaction', {
+        clientId: 1,
         paymentMethod: 'debit_card',
         amount: 1000
       })
       const payableModel = {
         create: (value) => Promise.resolve(value)
       }
-      const payableService = new PayableService(payableModel, paymentConfig)
+      const payableService = new PayableService(payableModel, null, paymentConfig)
 
       const payable = await payableService.store(transaction)
       const fee = ((1000 * paymentConfig.debit_card.percentageFee) / 100)
@@ -50,20 +45,20 @@ describe('Service: Payable', () => {
       expect(payable.fee).toBe(fee)
     })
     it('calcular o paymentDate com a configuração customizada', async () => {
-      const transaction = await factore.create('Transaction', {
-        clientId: client.id,
+      const transaction = await factore.attrs('Transaction', {
+        clientId: 1,
         paymentMethod: 'debit_card'
       })
       const payableModel = {
         create: (value) => Promise.resolve(value)
       }
-      const payableService = new PayableService(payableModel, paymentConfig)
+      const payableService = new PayableService(payableModel, null, paymentConfig)
       const payable = await payableService.store(transaction)
-      expect(payable.paymentDate)
+      expect(moment(payable.paymentDate).format('DDMMYYYY'))
         .toBe(
           moment()
             .add(paymentConfig.debit_card.amountDay, 'days')
-            .format('L')
+            .format('DDMMYYYY')
         )
     })
   })
